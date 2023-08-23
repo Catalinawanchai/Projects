070509ce5dfae89144f3126a446a869f53899876
Catalinawanchai <wanchaicatalina@gmail.com>
2023-08-23 14:51:17 -0700

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Biography from "./pages/Biography";
+import Films from "./pages/Films";
+import Plays from "./pages/Plays";
+import Press from "./pages/Press";
+import Contact from "./pages/Contact";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import SocialMediaBar from './components/SocialMediaBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path = "" element = {<Home/>} />
+        <Route path = "biography" element =  {<Biography/>}/>
+        <Route path = "plays" element =  {<Plays/>}/>
+        <Route path = "films" element =  {<Films/>}/>
+        <Route path = "press" element =  {<Press/>}/>
+        <Route path = "contact" element =  {<Contact/>}/>
+      </Routes>
+      </Router>
+    
+      <SocialMediaBar/>
+    </div>
+  );
+}
+
+export default App;
